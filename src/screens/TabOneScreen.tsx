7af02bb015119d0575cfa0d57a1f5968d8ea40e4
YauheniIhnatsyeu <Yauheni_Ihnatsyeu@epam.com>
2020-12-02 18:21:09 +0300

    save

--- src/screens/TabOneScreen.tsx
+++ src/screens/TabOneScreen.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import MapView from '../../components/Map/MapView';
 
 
 
@@ -9,17 +10,17 @@ import {MyMapComponent} from '../../components/Map';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor={"#eee"} darkColor="rgba(255,255,255,0.1)" />
 
-      <MyMapComponent isMarkerShown={true} />
-
       <Button title="Arr" onPress={()=>{}}/>
+      <MapView defaultZoom={15} region={{ latitude: 48.86, longitude: 2.34 }}  />
+
       
 
       <EditScreenInfo path="/screens/TabOneScreen.js" />
-    </View>
+    </ScrollView>
   );
 }
 
