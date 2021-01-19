61975c186834c211138bd92aa96b19d5514df628
Yahor <sheynikovea@gmail.com>
2021-01-19 17:22:21 +0300

    Fix marker shadows. Minimize magic constants and fix ts errors in CustomMarker.

--- src/components/Map/CustomMarker.tsx
+++ src/components/Map/CustomMarker.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, StyleProp, ViewStyle } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 // Marker status within current route progress
 export enum MarkerStatus {
@@ -12,20 +12,51 @@ interface Props {
   index: number;
   status: MarkerStatus;
 }
+
+const MARKER_RADIUS = 24;
+const TAIL_LENGTH = 5;
+const CIRCLE_PADDING = 2;
+
 export default function CustomMarker({ index, status }: Props) {
   const styles = React.useMemo(() => {
-    const colors = getMarkerColors(status); 
+    const colors = getMarkerColors(status);
     return StyleSheet.create({
+      wrapper: {
+        position: "relative",
+        transform: [
+          { translateX: -MARKER_RADIUS },
+          { translateY: -MARKER_RADIUS * 2 - TAIL_LENGTH },
+        ],
+        borderRadius: MARKER_RADIUS,
+        // shadow
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.6,
+        shadowRadius: 6,
+        elevation: 5,
+      },
       outer: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        position: "relative",
-        padding: 2,
-        width: 48,
-        height: 48,
-        transform: "translate(-50%, -100%)",
-        borderRadius: "50%",
+        padding: CIRCLE_PADDING,
+        width: MARKER_RADIUS * 2,
+        height: MARKER_RADIUS * 2,
+        borderRadius: MARKER_RADIUS,
+        backgroundColor: "#fff",
+      },
+      tail: {
+        position: "absolute",
+        bottom: 1,
+        left: "50%",
+        transform: [
+          { translateX: -TAIL_LENGTH },
+          { translateY: TAIL_LENGTH },
+          { rotate: "45deg" },
+        ],
+        width: TAIL_LENGTH * 2,
+        height: TAIL_LENGTH * 2,
+        borderBottomRightRadius: 2,
         backgroundColor: "#fff",
         // shadow
         shadowColor: "#000",
@@ -34,63 +65,51 @@ export default function CustomMarker({ index, status }: Props) {
         shadowRadius: 6,
         elevation: 5,
       },
-      tail: {
-        position: "absolute",
-        bottom: 2,
-        left: "50%",
-        transform: "translate(-50%, 50%) rotate(45deg)",
-        width: 10,
-        height: 10,
-        borderRadius: 2,
-        backgroundColor: "#fff",
-        // shadow
-        // shadowColor: "#000",
-        // shadowOffset: { width: 0, height: 3 },
-        // shadowOpacity: 0.6,
-        // shadowRadius: 6,
-        // elevation: 5,
-      },
       inner: {
         width: "100%",
         height: "100%",
         justifyContent: "center",
         alignItems: "center",
-        borderRadius: "50%",
-        ...colors
+        borderRadius: MARKER_RADIUS - CIRCLE_PADDING / 2,
+        ...colors,
       },
       text: {
         ...colors,
-        fontSize: 24
-      }
+        fontSize: 24,
+      },
     });
   }, [status]);
 
   return (
-    <View style={styles.outer}>
+    <View style={styles.wrapper}>
       <View style={styles.tail}></View>
-      <View style={styles.inner}>
-        <Text style={styles.text}>{index}</Text>
+      <View style={styles.outer}>
+        <View style={styles.inner}>
+          <Text style={styles.text}>{index}</Text>
+        </View>
       </View>
     </View>
   );
 }
 
-function getMarkerColors(status: MarkerStatus): { backgroundColor: string; color: string } {
+function getMarkerColors(
+  status: MarkerStatus
+): { backgroundColor: string; color: string } {
   switch (status) {
     case MarkerStatus.Visited:
       return {
-        backgroundColor: '#0bbb65',
-        color: '#fff',
+        backgroundColor: "#0bbb65",
+        color: "#fff",
       };
     case MarkerStatus.Current:
       return {
-        backgroundColor: '#fee900',
-        color: '#000',
+        backgroundColor: "#fee900",
+        color: "#000",
       };
     case MarkerStatus.Upcoming:
       return {
-      backgroundColor: '#6c717a',
-      color: '#fff',
-    };
+        backgroundColor: "#6c717a",
+        color: "#fff",
+      };
   }
 }
